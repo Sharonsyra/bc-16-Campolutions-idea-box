dbe605634e1bf7c5ade2c04d38f061a375a116f4
sharonsyra <sharonwaithii@gmail.com>
2017-03-13 19:59:52 +0300

    integrated url_for function and requests

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
@@ -12,10 +12,10 @@ DBSession = sessionmaker(bind = engine)
 session = DBSession()
 
 @app.route('/')
-@app.route('/users/<int:user_id>/')
-def user(user_id):
-	user = session.query(User).filter_by(id = user_id).one()
-	idea = session.query(Ideas).filter_by(user_id = user.id)
+def DefaultUser():
+	user = session.query(User).first()
+	idea = session.query(Ideas).filter_by(user_id = user_id)
+
 	output = ''
 	for i in idea:
 		output += i.name
@@ -30,12 +30,26 @@ def user(user_id):
 
 	return output
 
-def newIdea(user_id):
-	return "page to create new idea"
+@app.route('/users/<int:user_id>/')
+def user(user_id):
+	user = session.query(User).filter_by(id = user_id).one()
+	idea = session.query(Ideas).filter_by(user_id = user.id)
+	return render_template('user.html', user = user, idea = idea)
 
+@app.route('/users/<int:user_id>/new/', methods = ['GET', 'POST'])
+def newIdea(user_id):
+	if request.method == 'POST':
+		newIdea = Ideas(name = request.form['name'], user_id = user_id)
+		session.add(newIdea)
+		session.commit()
+		return redirect(url_for('user', user_id = user_id)) 
+	else:
+		return render_template('newIdea.html', user_id = user_id)
+@app.route('/users/<int:user_id>/edit/')
 def editIdea(user_id, idea_id):
 	return "page to edit an idea"
 
+@app.route('/users/<int:user_id>/<int:idea_id>/delete/')
 def deleteIdea():
 	return "Page to delete idea"
 
