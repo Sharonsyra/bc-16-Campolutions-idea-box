cee5699393b0faff3c0219dc0d6d6722c18ece51
sharonsyra <sharonwaithii@gmail.com>
2017-03-13 17:35:02 +0300

    added iteration to return the table ids and crud functions

--- project.py
+++ project.py
@@ -3,26 +3,41 @@ app = Flask(__name__)
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, User, Ideas
+from database_setup import Base, User, Ideas, Comments
 
-engine = create_engine('sqlite:///i.db')
+engine = create_engine('sqlite:///userideas.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind = engine)
 session = DBSession()
 
 @app.route('/')
-@app.route('/hello')
-def HelloWorld():
-	user = session.query(User).first()
+@app.route('/users/<int:user_id>/')
+def user(user_id):
+	user = session.query(User).filter_by(id = user_id).one()
 	idea = session.query(Ideas).filter_by(user_id = user.id)
 	output = ''
 	for i in idea:
 		output += i.name
 		output += '</br>'
+		output += i.description
+		output += '</br>'
+		output += i.category
+		output += '</br>'
+		output += i.tags
+		output += '</br>'
+		output += '</br>'
+
 	return output
 
+def newIdea(user_id):
+	return "page to create new idea"
 
+def editIdea(user_id, idea_id):
+	return "page to edit an idea"
+
+def deleteIdea():
+	return "Page to delete idea"
 
 if __name__ == '__main__':
-	app.run(debug = True)
+ 	app.run(debug = True)
